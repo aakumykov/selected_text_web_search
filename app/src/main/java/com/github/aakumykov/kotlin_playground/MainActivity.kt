465fd25d317700d991e6f4b976611bdccba92479
Andrey Kumykov <aakumykov@yandex.ru>
2023-11-24 06:54:49 +0300

    Класс-синглтон Logger и область вывода его сообщений.

--- app/src/main/java/com/github/aakumykov/kotlin_playground/MainActivity.kt
+++ app/src/main/java/com/github/aakumykov/kotlin_playground/MainActivity.kt
@@ -10,15 +10,25 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        Logger.messages.observe(this) { messages -> binding.logView.text = messages.joinToString("\n") }
+
         super.onCreate(savedInstanceState)
+        Logger.d(TAG, "onCreate()")
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.clearLogButton.setOnClickListener { Logger.clear() }
 
         binding.button.setOnClickListener { action1() }
         binding.button2.setOnClickListener { action2() }
         binding.button3.setOnClickListener { action3() }
         binding.button4.setOnClickListener { action4() }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Logger.d(TAG, "onDestroy()")
     }
 
     private fun action1() {
@@ -35,5 +45,9 @@ class MainActivity : AppCompatActivity() {
 
     private fun action4() {
         showToast("Привет 4")
+    }
+
+    companion object {
+        val TAG: String = MainActivity::class.java.simpleName
     }
 }
