096d45c214aa45375328226e4ad6bef1d877de40
Andrey Kumykov <aakumykov@yandex.ru>
2024-06-05 12:16:10 +0300

    Проскроллить WebView из AccessibilityService пока не получилось.

--- app/src/main/java/com/github/aakumykov/kotlin_playground/MyAccessibilityService.kt
+++ app/src/main/java/com/github/aakumykov/kotlin_playground/MyAccessibilityService.kt
@@ -1,37 +1,10 @@
 package com.github.aakumykov.kotlin_playground
 
 import android.accessibilityservice.AccessibilityService
-import android.content.Intent
 import android.view.accessibility.AccessibilityEvent
 import android.view.accessibility.AccessibilityNodeInfo
-import android.widget.Toast
-import androidx.core.os.bundleOf
-import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.supervisorScope
-import kotlinx.coroutines.withContext
-import java.util.concurrent.TimeUnit
-import kotlin.random.Random
 
 class MyAccessibilityService : AccessibilityService() {
-
-    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        intent?.apply {
-            when(action) {
-                ACTION_SCROLL_DOWN -> scrollDown()
-                ACTION_SCROLL_UP -> scrollUp()
-                else -> {}
-            }
-        }
-        return super.onStartCommand(intent, flags, startId)
-    }
-
-    private fun scrollUp() {
-
-    }
-
-    private fun scrollDown() {
-
-    }
 
     override fun onServiceConnected() {
         super.onServiceConnected()
@@ -44,22 +17,56 @@ class MyAccessibilityService : AccessibilityService() {
     override fun onInterrupt() {}
 
     override fun onAccessibilityEvent(event: AccessibilityEvent?) {
+        rootInActiveWindow?.also { rootView ->
+            debugLog("--------------")
+            debugLog("rootView.packageName: ${rootView.packageName}, childCount: ${rootView.childCount}")
 
-        val rootView = rootInActiveWindow
+            showChildrenRecursively(0, rootView.getChildren())
 
-        debugLog("--------------")
-        debugLog("rootView.packageName: ${rootView.packageName}, childCount: ${rootView.childCount}")
+            /*rootView.getChildren().first { node ->
+                isBrowserWebView(node, WEB_PAGE_TITLE)
+            }.also {
+                simpleNodeInfo(it)
+            }*/
+        }
+    }
 
-        rootView.apply {
-            for (i in 0..<childCount) {
-                val child = getChild(i)
-                val className = child.className
-                val text = child.text
-                val desc = child.contentDescription
-                debugLog("┝━━ i=$i, className: $className, text: $text, desc: $desc")
+    fun isBrowserWebView(node: AccessibilityNodeInfo?, text: String): Boolean {
+        return node?.let {
+            "android.webkit.WebView" == it.className
+                    && (it.text?.let { t -> t.equals(text) } ?: false)
+        } ?: false
+    }
+
+    private fun showChildrenRecursively(level: Int, list: List<AccessibilityNodeInfo>) {
+        list.forEachIndexed { i, node ->
+
+            showNodeInfo(level, i, node)
+
+            node?.getChildren()?.also { childList: List<AccessibilityNodeInfo> ->
+                if (childList.isNotEmpty())
+                    showChildrenRecursively(level+1, childList)
             }
         }
+    }
 
+    private fun showNodeInfo(level: Int, index: Int, node: AccessibilityNodeInfo) {
+        node?.apply {
+            val indent = "━━".repeat(level)
+            debugLog("┝$indent i=$index, ${simpleNodeInfo(node)}")
+        }
+    }
+
+    private fun simpleNodeInfo(node: AccessibilityNodeInfo): String {
+
+        if (isBrowserWebView(node, WEB_PAGE_TITLE)) {
+            debugLog("Найден WebView с '${WEB_PAGE_TITLE}'")
+            node?.performAction(AccessibilityNodeInfo.ACTION_SCROLL_FORWARD)
+        }
+
+        return with(node) {
+            "className: ${className}, text: $text, desc: $contentDescription"
+        }
     }
 
     private fun debugLog(text: String) {
@@ -70,5 +77,13 @@ class MyAccessibilityService : AccessibilityService() {
         val TAG: String = MyAccessibilityService::class.java.simpleName
         const val ACTION_SCROLL_DOWN: String = "ACTION_SCROLL_DOWN"
         const val ACTION_SCROLL_UP: String = "ACTION_SCROLL_UP"
+        const val WEB_PAGE_TITLE: String = "Проза.ру"
+    }
+}
+
+fun AccessibilityNodeInfo.getChildren(): List<AccessibilityNodeInfo> {
+    return when(childCount) {
+        0 -> emptyList()
+        else -> MutableList(childCount) { getChild(it) }
     }
 }
