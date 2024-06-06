802dd2a410cf698637308aed848e868c89f50ecc
Andrey Kumykov <aakumykov@yandex.ru>
2024-06-06 22:22:40 +0300

    Сервер, откликающийся по HTTP.

--- ktor_server/src/main/java/com/github/aakumykov/ktor_server/ServerActivity.kt
+++ ktor_server/src/main/java/com/github/aakumykov/ktor_server/ServerActivity.kt
@@ -5,16 +5,44 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.github.aakumykov.ktor_server.databinding.ActivityServerBinding
+import io.ktor.server.application.Application
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class ServerActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityServerBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        prepareLayout()
+        binding.startServerButton.setOnClickListener { startServer() }
+    }
+
+    private fun startServer() {
+        CoroutineScope(Dispatchers.IO).launch {
+            embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
+                .start(wait = true)
+        }
+    }
+
+    private fun prepareLayout() {
+        binding = ActivityServerBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_server)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
     }
+}
+
+fun Application.module() {
+    configureSockets()
+    configureRouting()
 }
---
+++ ktor_server/src/main/java/com/github/aakumykov/ktor_server/Sockets.kt
@@ -0,0 +1,30 @@
+package com.github.aakumykov.ktor_server
+
+import io.ktor.server.application.Application
+import io.ktor.server.application.install
+import io.ktor.server.routing.routing
+import io.ktor.server.websocket.WebSockets
+import io.ktor.server.websocket.webSocket
+import io.ktor.websocket.Frame
+import io.ktor.websocket.readText
+import io.ktor.websocket.send
+
+fun Application.configureSockets() {
+    install(WebSockets) {
+//        pingPeriod = Duration.ofSeconds(15)
+//        timeout = Duration.ofSeconds(15)
+//        maxFrameSize = Long.MAX_VALUE
+//        masking = false
+    }
+
+    routing {
+        webSocket("/chat") {
+            send("Вы подключились!")
+            for(frame in incoming) {
+                frame as? Frame.Text ?: continue
+                val receivedText = frame.readText()
+                send("Вы написали: $receivedText")
+            }
+        }
+    }
+}
