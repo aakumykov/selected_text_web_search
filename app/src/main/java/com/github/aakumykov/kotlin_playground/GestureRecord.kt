faf4e5724df27a63e7d4d399a42cfa55959185bc
Andrey Kumykov <aakumykov@yandex.ru>
2024-06-06 07:24:25 +0300

    fix: копирую элементы массива в новый объект GestureRecord вместо передачи ссылки на них

--- app/src/main/java/com/github/aakumykov/kotlin_playground/GestureRecord.kt
+++ app/src/main/java/com/github/aakumykov/kotlin_playground/GestureRecord.kt
@@ -8,37 +8,48 @@ class GestureRecord private constructor(
     private val startingTime: Long,
     private val endingTime: Long
 ) {
-    fun createGestureDescription(): GestureDescription {
-        return GestureDescription.Builder().apply {
-            addStroke(createStrokeDescription())
-        }.build()
-    }
-
-    private fun createPath(): Path {
-        return Path().apply {
-            pointList.firstOrNull()?.also {
-                moveTo(it.fromX, it.fromY)
-            }
-            if (pointList.size>1) {
-                pointList.subList(1, pointList.lastIndex).also { sublist ->
-                    sublist.forEach { gp ->
-                        lineTo(gp.toX, gp.toY)
-                    }
-            }}
+    fun createGestureDescription(): GestureDescription? {
+        return createStrokeDescription()?.let { stroke ->
+            GestureDescription.Builder().apply {
+                addStroke(stroke)
+            }.build()
         }
     }
 
-    private fun createStrokeDescription(): GestureDescription.StrokeDescription {
-        return GestureDescription.StrokeDescription(
-            createPath(),
-            0L,
-            endingTime - startingTime
-        )
+    private fun createPath(): Path? {
+
+        if (pointList.size <= 1)
+            return null
+
+        return Path().apply {
+            pointList.first().also {
+                moveTo(it.fromX, it.fromY)
+            }
+            pointList.subList(1, pointList.lastIndex).forEach { gp ->
+                lineTo(gp.toX, gp.toY)
+            }
+        }
+    }
+
+    private fun createStrokeDescription(): GestureDescription.StrokeDescription? {
+        return createPath()?.let { path ->
+            return GestureDescription.StrokeDescription(
+                path,
+                0L,
+                endingTime - startingTime
+            )
+        }
     }
 
     companion object {
-        fun create(pointList: List<GesturePoint>, startingTime: Long, endingTime: Long): GestureRecord {
-            return GestureRecord(pointList, startingTime, endingTime)
+        fun create(pointList: List<GesturePoint>, startingTime: Long, endingTime: Long): GestureRecord? {
+            return if (pointList.size > 1) {
+                GestureRecord(
+                    ArrayList(pointList),
+                    startingTime,
+                    endingTime
+                )
+            } else null
         }
     }
 }
