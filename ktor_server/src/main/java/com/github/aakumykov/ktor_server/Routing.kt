54909b29b237e36c018d419eecbc64fdcaf3fe8c
Andrey Kumykov <aakumykov@yandex.ru>
2024-06-07 10:51:46 +0300

    Добавляет время-дату в ответ HTTP-сервера.

--- ktor_server/src/main/java/com/github/aakumykov/ktor_server/Routing.kt
+++ ktor_server/src/main/java/com/github/aakumykov/ktor_server/Routing.kt
@@ -5,11 +5,19 @@ import io.ktor.server.application.call
 import io.ktor.server.response.respondText
 import io.ktor.server.routing.get
 import io.ktor.server.routing.routing
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
-fun Application.configureRouting() {
+fun Application.configureHttpRouting() {
+
     routing {
         get("/") {
-            call.respondText("Привет от Ktor!")
+            call.respondText("Привет от Ktor! Время ${dateTime()}")
         }
     }
+}
+
+fun dateTime(): String {
+    return SimpleDateFormat("hhч mmм ssс, dd.MM.yy", Locale.getDefault()).format(Date())
 }
