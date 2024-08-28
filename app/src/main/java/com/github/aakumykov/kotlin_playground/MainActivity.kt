b926ecb64b559ca16047557a9a4376ef048c3f77
Andrey Kumykov <aakumykov@yandex.ru>
2024-08-28 20:27:59 +0300

    Вынес метод showAppProperties в расширение, упростил код MainActivity.

--- app/src/main/java/com/github/aakumykov/kotlin_playground/MainActivity.kt
+++ app/src/main/java/com/github/aakumykov/kotlin_playground/MainActivity.kt
@@ -1,12 +1,9 @@
 package com.github.aakumykov.kotlin_playground
 
-import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
-import android.provider.Settings
 import androidx.appcompat.app.AppCompatActivity
 import com.github.aakumykov.kotlin_playground.databinding.ActivityMainBinding
-import com.github.aakumykov.kotlin_playground.extensions.LogD
+import com.github.aakumykov.kotlin_playground.extensions.showAppProperties
 import com.github.aakumykov.kotlin_playground.extensions.showToast
 
 class MainActivity : AppCompatActivity() {
@@ -15,11 +12,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         Logger.messages.observe(this) { messages -> binding.logView.text = messages.joinToString("\n") }
-
         super.onCreate(savedInstanceState)
-        Logger.d(TAG, "onCreate()")
-
-        LogD("onCreate()")
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -33,10 +26,6 @@ class MainActivity : AppCompatActivity() {
         binding.button4.setOnClickListener { action4() }
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        Logger.d(TAG, "onDestroy()")
-    }
 
     private fun action1() {
         showToast("Привет :-)")
@@ -54,11 +43,10 @@ class MainActivity : AppCompatActivity() {
         showToast("Привет 4")
     }
 
-    private fun showAppProperties() {
-        val uri = Uri.parse("package:$packageName")
-        val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS, uri)
-        if (intent.resolveActivity(packageManager) != null) { startActivity(intent) }
-    }
+
+
+    private fun log(text:String) = Logger.d(TAG, text)
+
 
     companion object {
         val TAG: String = MainActivity::class.java.simpleName
--- app/src/main/java/com/github/aakumykov/kotlin_playground/extensions/ActivityExtensions.kt
+++ app/src/main/java/com/github/aakumykov/kotlin_playground/extensions/ActivityExtensions.kt
@@ -1,8 +1,12 @@
 package com.github.aakumykov.kotlin_playground.extensions
 
 import android.app.Activity
+import android.content.Intent
+import android.net.Uri
+import android.provider.Settings
 import android.widget.Toast
 import androidx.annotation.StringRes
+import androidx.core.content.ContextCompat.startActivity
 
 fun Activity.showToast(text: String) {
     Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
@@ -10,4 +14,10 @@ fun Activity.showToast(text: String) {
 
 fun Activity.showToast(@StringRes strRes: Int) {
     showToast(getString(strRes))
+}
+
+fun Activity.showAppProperties() {
+    val uri = Uri.parse("package:$packageName")
+    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS, uri)
+    if (intent.resolveActivity(packageManager) != null) { startActivity(intent) }
 }
