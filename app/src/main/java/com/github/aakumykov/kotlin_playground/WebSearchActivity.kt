1c3002aec0b467831bb557ee8c72bb1a726dd35a
Andrey Kumykov <aakumykov@yandex.ru>
2024-10-15 14:30:02 +0300

    Проба-1

---
+++ app/src/main/java/com/github/aakumykov/kotlin_playground/WebSearchActivity.kt
@@ -0,0 +1,42 @@
+package com.github.aakumykov.kotlin_playground
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.github.aakumykov.kotlin_playground.databinding.ActivityMainBinding
+
+class WebSearchActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        processIntentAndFinish(intent)
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        processInputIntent(intent)
+    }
+
+    private fun processIntentAndFinish(intent: Intent?) {
+        processInputIntent(intent)
+        finish()
+    }
+
+    private fun processInputIntent(intent: Intent?) {
+        intent?.getCharSequenceExtra(Intent.EXTRA_PROCESS_TEXT)?.toString()?.also { text ->
+            binding.selectedTextView.text = text
+            Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    companion object {
+        val TAG: String = WebSearchActivity::class.java.simpleName
+    }
+}
