4b37aa40158237a13daea1fca1516acfb6f6b061
Andrey Kumykov <aakumykov@yandex.ru>
2024-10-15 14:37:25 +0300

    Удалил "material" и "app compat".

--- app/src/main/java/com/github/aakumykov/kotlin_playground/WebSearchActivity.kt
+++ app/src/main/java/com/github/aakumykov/kotlin_playground/WebSearchActivity.kt
@@ -1,12 +1,12 @@
 package com.github.aakumykov.kotlin_playground
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
 import com.github.aakumykov.kotlin_playground.databinding.ActivityMainBinding
 
-class WebSearchActivity : AppCompatActivity() {
+class WebSearchActivity : Activity() {
 
     private lateinit var binding: ActivityMainBinding
 
